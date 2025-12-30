1641f3ace89921b17c2bf8685f49f3324eb87db9
Algeria <funsoft.technologies@gmail.com>
2025-12-30 07:04:59 +0100

    Mise à jour configuration projet et module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,24 +1,39 @@
+//module app {
+//
+//    // JavaFX
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//
+//
+//    requires java.desktop;
+//    requires static lombok;
+//    requires org.apache.pdfbox;
+//    requires javafx.swing;
+//    requires com.fasterxml.jackson.databind;
+//    requires com.fasterxml.jackson.datatype.jsr310;
+//
+//    // JavaFX FXML
+//    opens app to javafx.fxml;
+//
+//    // OUVERTURE POUR JACKSON (OBLIGATOIRE)
+//    opens app.model to com.fasterxml.jackson.databind;
+//
+//    // Exports normaux
+//    exports app;
+//    exports app.model;
+//}
 module app {
+        requires javafx.controls;
+        requires javafx.fxml;
+        requires static lombok;
+        requires com.fasterxml.jackson.annotation;
+        requires javafx.swing;
+        requires org.apache.pdfbox;
+        requires com.fasterxml.jackson.databind;
+        requires com.fasterxml.jackson.datatype.jsr310;
 
-    // JavaFX
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    requires java.desktop;
-    requires static lombok;
-    requires org.apache.pdfbox;
-    requires javafx.swing;
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.datatype.jsr310;
-
-    // JavaFX FXML
-    opens app to javafx.fxml;
-
-    // OUVERTURE POUR JACKSON (OBLIGATOIRE)
-    opens app.model to com.fasterxml.jackson.databind;
-
-    // Exports normaux
-    exports app;
-    exports app.model;
-}
+        // OUVERTURE POUR JACKSON (OBLIGATOIRE)
+        opens app.model to com.fasterxml.jackson.databind;
+        opens app to javafx.fxml;
+        exports app;
+        }
